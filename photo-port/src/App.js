d33228c03bf6cf4dabe5df2c99ba95b9ebe7425d
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-07-10 22:06:13 -0400

    submit button

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -19,14 +20,17 @@ function App() {
 
   return (
     <div>
+      
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
       ></Nav>
       <main>
+      <ContactForm />
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
+        
       </main>
     </div>
   );
